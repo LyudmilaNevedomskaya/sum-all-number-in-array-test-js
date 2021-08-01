f7859bbaec1a40f448621b3385473033ae69647d
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-08-01 08:05:54 +0000

    Recursion Sum Array of Numbers

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      sum += sumItems(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
 }
 
+
+
+
+
+// console.log(sumItems([1, 3, 3, 5, 5]));
+// console.log(sumItems([12]));
+// console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 module.exports = sumItems;
